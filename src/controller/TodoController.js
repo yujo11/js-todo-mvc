ebba1c409f0d8d926220f0862cdc3d7ced96d7e1
조윤호 <yujo.dev@gmail.com>
2023-01-07 15:44:40 +0900

    feat: Controller와 View 연결

--- src/controller/TodoController.js
+++ src/controller/TodoController.js
@@ -1,7 +1,12 @@
+import Controller from "./Controller.js";
 import Todo from "../model/Todo.js";
+import TodoView from "../view/TodoView.js";
 
-const TodoController = class {
-  constructor() {
+const TodoController = class extends Controller {
+  constructor(...arg) {
+    super(...arg);
+
+    this.todoView = new TodoView();
     this.todos = [];
   }
 
@@ -11,22 +16,34 @@ const TodoController = class {
 
   add(title) {
     this.todos.push(new Todo(title));
+
+    this.todoView.renderAll(this.todos);
+    this.todoView.renderCount(this.todos.length);
   }
 
   remove(index) {
     this.todos.splice(index, 1);
+
+    this.todoView.renderAll(this.todos);
+    this.todoView.renderCount(this.todos.length);
   }
 
   toggle(index) {
     this.todos[index].toggle();
+
+    this.todoView.renderAll(this.todos);
   }
 
   edit(index) {
     this.todos[index].edit();
+
+    this.todoView.renderAll(this.todos);
   }
 
   save(index, title) {
     this.todos[index].save(title);
+
+    this.todoView.renderAll(this.todos);
   }
 };
 
