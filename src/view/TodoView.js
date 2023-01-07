b6867ad46105a84f6aed9cf2919a48bd45388d86
조윤호 <yujo.dev@gmail.com>
2023-01-07 16:19:54 +0900

    refactor: template 분리

--- src/view/TodoView.js
+++ src/view/TodoView.js
@@ -11,27 +11,8 @@ const TodoView = class {
     this.$todoList.innerHTML = todos
       .map((todo, index) => {
         return todo.editing
-          ? `
-            <li data-index="${index}" class="editing">
-              <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label class="label">완료된 타이틀</label>
-                <button class="destroy"></button>
-              </div>
-              <input class="edit" value=${todo.title} />
-            </li>
-        `
-          : `
-            <li data-index="${index}" class="todo-item  ${
-              todo.completed && "completed"
-            }">
-              <input type="checkbox" class="toggle" ${
-                todo.completed && "checked"
-              } />
-              <label class="label">${todo.title}</label>
-              <button class="destroy"></button>
-            </li>
-        `;
+          ? this.#editTemplate(todo, index)
+          : this.#todoTemplate(todo, index);
       })
       .join("");
   }
@@ -48,6 +29,27 @@ const TodoView = class {
       .forEach(($filter) => $filter.classList.remove("selected"));
 
     $filter.classList.add("selected");
+  }
+
+  #editTemplate(todo, index) {
+    return `<li data-index="${index}" class="editing">
+              <div class="view">
+                <input class="toggle" type="checkbox" />
+                <label class="label">완료된 타이틀</label>
+                <button class="destroy"></button>
+              </div>
+              <input class="edit" value=${todo.title} />
+            </li>`;
+  }
+
+  #todoTemplate(todo, index) {
+    return `<li data-index="${index}" class="todo-item
+                 ${todo.completed && "completed"}">
+               <input type="checkbox" class="toggle"
+                ${todo.completed && "checked"} />
+               <label class="label">${todo.title}</label>
+               <button class="destroy"></button>
+            </li>`;
   }
 };
 
