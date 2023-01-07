4256740aed2a88c36ee800a6da2afcfa3b2477ac
조윤호 <yujo.dev@gmail.com>
2023-01-07 14:20:44 +0900

    feat: Todo Model

---
+++ src/model/Todo.js
@@ -0,0 +1,20 @@
+class Todo {
+  constructor(title) {
+    this.title = title;
+    this.completed = false;
+    this.isEditing = false;
+  }
+
+  toggle() {
+    this.completed = !this.completed;
+  }
+
+  edit() {
+    this.isEditing = true;
+  }
+
+  save(title) {
+    this.title = title;
+    this.isEditing = false;
+  }
+}
