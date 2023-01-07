7b8b47ea4e7d50ad49ac76cbf2e8c7c52e8d6449
조윤호 <yujo.dev@gmail.com>
2023-01-07 14:24:30 +0900

    feat: Todos Model

---
+++ src/model/Todos.js
@@ -0,0 +1,31 @@
+import Todo from "./Todo.js";
+
+class Todos {
+  constructor() {
+    this.todos = [];
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  add(title) {
+    this.todos.push(new Todo(title));
+  }
+
+  remove(index) {
+    this.todos.splice(index, 1);
+  }
+
+  toggle(index) {
+    this.todos[index].toggle();
+  }
+
+  edit(index) {
+    this.todos[index].edit();
+  }
+
+  save(index, title) {
+    this.todos[index].save(title);
+  }
+}
