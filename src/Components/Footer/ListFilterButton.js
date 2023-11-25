01337362863a06de6c65f38d58450a86a84d7600
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-26 01:19:40 +0900

    [Refactoring] 현재 필터 항목 표시를 위한 수정

--- src/Components/Footer/ListFilterButton.js
+++ src/Components/Footer/ListFilterButton.js
@@ -4,11 +4,10 @@ export default function ListFilterButton({ target, option, text, isDefault }) {
   target.appendChild(liElement);
 
   const inputElement = document.createElement("input");
-  inputElement.setAttribute("id", option);
+  inputElement.setAttribute("id", `radio_${option}`);
   inputElement.setAttribute("class", "footer__filter-input");
   inputElement.setAttribute("type", "radio");
   inputElement.setAttribute("name", "filterOption");
-  inputElement.setAttribute("value", option);
 
   if (isDefault) {
     inputElement.setAttribute("checked", "true");
@@ -18,7 +17,8 @@ export default function ListFilterButton({ target, option, text, isDefault }) {
 
   const labelElement = document.createElement("label");
   labelElement.setAttribute("class", "footer__filter-label");
-  labelElement.setAttribute("for", option);
+  labelElement.setAttribute("for", `radio_${option}`);
+  labelElement.setAttribute("data-value", option);
   labelElement.innerText = text;
 
   liElement.appendChild(labelElement);
