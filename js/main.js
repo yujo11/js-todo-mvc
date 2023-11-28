393e23c74981d08427acfedbf21e4ebe510d1478
최훈오 <gnsdh86166@gmail.com>
2023-11-28 14:54:35 +0900

    [Refactor] count 계산 로직분리

--- js/main.js
+++ js/main.js
@@ -1,86 +1,88 @@
-import { getItem, setItem } from "../utils/storage.js";
+import { getItem, setItem } from "../utils/storage.js"
 
-const $todoInput = document.querySelector(".new-todo");
-const $todoList = document.querySelector(".todo-list");
-const $todoCount = document.querySelector(".todo-count");
+const $todoInput = document.querySelector(".new-todo")
+const $todoList = document.querySelector(".todo-list")
+const $todoCount = document.querySelector(".todo-count")
 
 const init = () => {
-	initEventListener();
-	render();
-};
+	initEventListener()
+	render()
+}
 
 const initEventListener = () => {
+	// 입력
 	$todoInput.addEventListener("keydown", (e) => {
 		// 공백일때 추가 x
 		if (!e.target.value) {
-			return;
+			return
 		}
 		if (e.key === "Enter") {
-			addTodo(e.target.value);
-			$todoInput.value = "";
+			addTodo(e.target.value)
+			$todoInput.value = ""
 		}
-	});
+	})
 
+	// 토글, 삭제
 	$todoList.addEventListener("click", (e) => {
-		const $li = e.target.closest("li");
+		const $li = e.target.closest("li")
 		if ($li) {
-			const className = e.target.classList;
-			const { id } = $li.dataset;
-			// 삭제
-			if (className.contains("destroy")) {
-				removeTodo(Number(id));
-			} else if (className.contains("toggle")) {
-				toggleTodo(Number(id));
+			const classList = e.target.classList
+			const { id } = $li.dataset
+			if (classList.contains("destroy")) {
+				removeTodo(Number(id))
+			} else if (classList.contains("toggle")) {
+				toggleTodo(Number(id))
 			}
 		}
-	});
+	})
 
+	// 더블클릭
 	$todoList.addEventListener("dblclick", (e) => {
-		const $li = e.target.closest("li");
+		const $li = e.target.closest("li")
 		if ($li) {
-			// li 태그 수정 모드 삭제
-			document
-				.querySelectorAll(".todo-list li.editing")
-				.forEach(($currentLi) => {
-					$currentLi.classList.remove("editing");
-				});
+			// 수정 모드 삭제
+			$todoList.querySelectorAll(".editing").forEach(($Li) => {
+				$Li.classList.remove("editing")
+			})
 
 			// 수정 모드
-			const $editInput = $li.querySelector(".edit");
-			const todoValue = $li.querySelector(".label").textContent;
-			$editInput.value = todoValue;
-			$li.classList.add("editing");
-			$editInput.focus();
+			const $editInput = $li.querySelector(".edit")
+			const todoValue = $li.querySelector(".label").textContent
+			$editInput.value = todoValue
+			$li.classList.add("editing")
+			$editInput.focus()
 		}
-	});
+	})
 
+	// 수정
 	$todoList.addEventListener("keydown", (e) => {
-		const $li = e.target.closest("li");
+		const $li = e.target.closest("li")
 		if ($li) {
 			if (e.key === "Enter") {
-				$li.classList.remove("editing");
-				editTodo(Number($li.dataset.id), e.target.value);
+				$li.classList.remove("editing")
+				editTodo(Number($li.dataset.id), e.target.value)
 			} else if (e.key === "Escape") {
-				$li.classList.remove("editing");
+				$li.classList.remove("editing")
 			}
 		}
-	});
-	window.addEventListener("hashchange", render);
-};
+	})
+
+	// 필터링
+	window.addEventListener("hashchange", render)
+}
 
 // 그리기
 const render = () => {
-	const currentTodo = getTodo();
-	const filterOption = getHashFilter();
-	const filteredTodo = filterTodo(currentTodo, filterOption);
-	const $todoCountValue = $todoCount.querySelector("strong");
-	$todoCountValue.textContent = filteredTodo.length; // 여기에 추가
-	updateUI(filteredTodo);
-};
+	const currentTodo = getTodo()
+	const filterOption = getHashFilter()
+	const filteredTodo = filterTodo(currentTodo, filterOption)
+	handleTodoCount(filteredTodo.length)
+	updateUI(filteredTodo)
+}
 
 const updateUI = (todos) => {
-	$todoList.innerHTML = todos.map((todo) => createHTML(todo)).join("");
-};
+	$todoList.innerHTML = todos.map((todo) => createHTML(todo)).join("")
+}
 
 const createHTML = (todo) => {
 	return `
@@ -94,74 +96,75 @@ const createHTML = (todo) => {
 			</div>
                 <input class="edit" value="새로운 타이틀" />
     </li>
-    `;
-};
+    `
+}
 
 const getHashFilter = () => {
-	const hash = window.location.hash.replace("#", "");
+	const hash = window.location.hash.replace("#", "")
 	switch (hash) {
 		case "active":
-			return "active";
+			return "active"
 		case "completed":
-			return "completed";
+			return "completed"
 		default:
-			return "all";
+			return "all"
 	}
-};
+}
 
 const filterTodo = (todos, filter) => {
 	switch (filter) {
 		case "active":
-			return todos.filter((todo) => !todo.completed);
+			return todos.filter((todo) => !todo.completed)
 		case "completed":
-			return todos.filter((todo) => todo.completed);
+			return todos.filter((todo) => todo.completed)
 		default:
-			return todos;
+			return todos
 	}
-};
+}
 
 const addTodo = (text) => {
-	const currentTodo = getTodo();
 	const updatedTodo = [
-		...currentTodo,
+		...getTodo(),
 		{
 			id: Date.now(),
 			text,
 			completed: false,
 		},
-	];
-	setTodo(updatedTodo);
-};
+	]
+	setTodo(updatedTodo)
+}
 
 const removeTodo = (id) => {
-	const currentTodo = getTodo();
-	const updatedTodo = currentTodo.filter((todo) => todo.id !== id);
-	setTodo(updatedTodo);
-};
+	const updatedTodo = getTodo().filter((todo) => todo.id !== id)
+	setTodo(updatedTodo)
+}
 
 const toggleTodo = (id) => {
-	const currentTodo = getTodo();
-	const updatedTodo = currentTodo.map((todo) =>
+	const updatedTodo = getTodo().map((todo) =>
 		todo.id === id ? { ...todo, completed: !todo.completed } : todo
-	);
-	setTodo(updatedTodo);
-};
+	)
+	setTodo(updatedTodo)
+}
 
 const editTodo = (id, todoValue) => {
-	const currentTodo = getTodo();
-	const updatedTodo = currentTodo.map((todo) =>
+	const updatedTodo = getTodo().map((todo) =>
 		todo.id === id ? { ...todo, text: todoValue } : todo
-	);
-	setTodo(updatedTodo);
-};
+	)
+	setTodo(updatedTodo)
+}
+
+const handleTodoCount = (count) => {
+	const $todoCountValue = $todoCount.querySelector("strong")
+	$todoCountValue.textContent = count // 여기에 추가
+}
 
 const getTodo = () => {
-	return getItem("todolist", []);
-};
+	return getItem("todolist", [])
+}
 
 const setTodo = (newTodos) => {
-	setItem("todolist", newTodos);
-	render();
-};
+	setItem("todolist", newTodos)
+	render()
+}
 
-init();
+init()
