d65e29780b8967e7fd0ff23522f9c84a7da7feaa
J <68155263+jxxxxe@users.noreply.github.com>
2023-11-30 08:28:05 +0900

    feat: Todo 입력창과 Todo 아이템 기능 구현

---
+++ src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import TodoItem from './TodoItem.js'
+
+export default function TodoForm({updateList, updateCount}) {
+    const $newTodoForm = document.querySelector('.new-todo-form')
+    const $todoList = document.querySelector('.todo-list')
+
+    $newTodoForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const $newTodoInput = $newTodoForm.querySelector('.new-todo')
+        const { value } = $newTodoInput
+
+        if (value) {
+            new TodoItem({
+                $target: $todoList,
+                title: value,
+                onDelete: (id, isToggled) => {
+                    const $deletedItem = document.getElementById(id)
+                    $todoList.removeChild($deletedItem)
+                    updateCount('delete', isToggled)
+                    updateList()
+                },
+                onToggle: (isCompleted) => { 
+                    updateCount('toggle', isCompleted) 
+                    updateList()
+                }
+            })
+
+            updateCount('add')
+            updateList()
+
+            $newTodoInput.value = ''
+        }
+    })
+}
